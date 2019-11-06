94b3c8fa67ba94713373c005e7b7a9b369029253
Robin <544768940@qq.com>
2019-11-07 03:20:28 +0800

    feat: redis option

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # koa2-weibo
 
 # koa2 -e koa2-weibo
+
+git commit -m 'refactor: 调整目录结构'
---
+++ src/cache/_redis.js
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+
+const redis = require('redis')
+const { REDIS_CONF } = require('../conf/db')
+
+const redisClient = redis.createClient(REDIS_CONF.port, REDIS_CONF.host)
+redisClient.on('error', err=>{
+    console.error('redis error', err);
+})
+
+//秒
+/**
+ * 
+ * @param {string} key 
+ * @param {string} val 
+ * @param {number} timeout 
+ */
+function set(key, val, timeout = 60*60){
+    if(typeof val === 'object'){
+        val = JSON.stringify(val)
+    }
+    redisClient.set(key,val)
+    redisClient.expire(key, timeout)
+}
+/**
+ * 
+ * @param {string} key 
+ */
+function get(key){
+    const promise = new Promise((resolve, reject)=>{
+        redisClient.get(key, (err, val)=>{
+            if(err){
+                reject(err)
+                return
+            }
+            if(val == null){
+                resolve(null)
+                return
+            }
+            try {
+                resolve(JSON.parse(val))
+            } catch (ex) {
+                resolve(val)
+            }
+        })
+    })
+    return promise
+}
+
+module.exports = {
+    set,
+    get
+}
